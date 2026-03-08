3649116d7d31305a189dacfb287136f49de18a6a
carlsoncompanyx <carlsoncompanyx@gmail.com>
2026-03-08 10:59:01 -0500

    Log Supabase environment variables in register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+console.log('NEXT_PUBLIC_SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('NEXT_PUBLIC_SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 import Link from 'next/link';
 import { useEffect, useMemo, useState } from 'react';
 
